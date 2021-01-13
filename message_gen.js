93616c58fc4d0a99cd8e5081b71cc3103a7138fe
VH <76993145+neverforgetusername@users.noreply.github.com>
2021-01-13 18:39:10 +0200

    let's see

--- message_gen.js
+++ message_gen.js
@@ -13,10 +13,10 @@ const thirdPartOfMessage = ["is the best answer", "should be considered", "won't
 
 //  Funktio, joka palauttaa viestin
 function generateMessage() {
-    let message = [];
-    message.push(firstPartOfMessage[Math.floor(Math.random() * firstPartOfMessage.length)]);
-    message.push(secondPartOfMessage[Math.floor(Math.random() * secondPartOfMessage.length)]);
-    message.push(thirdPartOfMessage[Math.floor(Math.random() * thirdPartOfMessage.length)]);
+    let message = "";
+    message = firstPartOfMessage[Math.floor(Math.random() * firstPartOfMessage.length)] + 
+    secondPartOfMessage[Math.floor(Math.random() * secondPartOfMessage.length)] +  
+    thirdPartOfMessage[Math.floor(Math.random() * thirdPartOfMessage.length)];
     return message;
 }
 
