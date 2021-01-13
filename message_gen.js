32f834b9b155dc9928d25f369d7e325bda3f9f7a
VH <76993145+neverforgetusername@users.noreply.github.com>
2021-01-13 18:32:24 +0200

    Function for making a array. Have to convert to string still.

--- message_gen.js
+++ message_gen.js
@@ -4,10 +4,21 @@ A Codeacademy project - message generator program. This code should
 randomly generate uninspirational messages. 
 */
 
-// Paikka josta hakea dataa haetaan viestiä varten.Helposti ymmärrettävä tapa.
+// Paikka josta hakea dataa haetaan viestiä varten. Helposti ymmärrettävä tapa.
 
-const firsPartOfMessage = ["Probably  ", "Nevertheless ", "Sometimes "];
+const firstPartOfMessage = ["Probably  ", "Nevertheless ", "Sometimes "];
 const secondPartOfMessage = ["quitting ", "cheating ", "sleeping "];
-const thirdPartOgMessage = ["is the best answer", "should be considered", "won't hurt you"];
+const thirdPartOfMessage = ["is the best answer", "should be considered", "won't hurt you"];
 
-console.log(thirdPartOgMessage[2]);
+
+//  Funktio, joka palauttaa viestin
+function generateMessage() {
+    let message = [];
+    message.push(firstPartOfMessage[Math.floor(Math.random() * firstPartOfMessage.length)]);
+    message.push(secondPartOfMessage[Math.floor(Math.random() * secondPartOfMessage.length)]);
+    message.push(thirdPartOfMessage[Math.floor(Math.random() * thirdPartOfMessage.length)]);
+    return message;
+}
+
+
+console.log(generateMessage());
