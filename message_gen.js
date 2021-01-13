6d3465cb490aae05f6780c34c6c1cb8db728b7c1
VH <76993145+neverforgetusername@users.noreply.github.com>
2021-01-13 10:22:11 +0200

    added message-bodies

--- message_gen.js
+++ message_gen.js
@@ -4,4 +4,10 @@ A Codeacademy project - message generator program. This code should
 randomly generate uninspirational messages. 
 */
 
-console.log("David Brent");
+// Paikka josta hakea dataa haetaan viestiä varten.Helposti ymmärrettävä tapa.
+
+const firsPartOfMessage = ["Probably  ", "Nevertheless ", "Sometimes "];
+const secondPartOfMessage = ["quitting ", "cheating ", "sleeping "];
+const thirdPartOgMessage = ["is the best answer", "should be considered", "won't not hurt you"];
+
+console.log(thirdPartOgMessage);
